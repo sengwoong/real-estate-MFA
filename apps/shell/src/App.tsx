bcee0419983b045b0857c413200ec15d8c1c192e
sengwoong <rej287573@gmail.com>
2024-09-02 02:47:26 +0900

    :sparkles: ui-kit icon 업로드

--- apps/shell/src/App.tsx
+++ apps/shell/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import './index.css'
+import { Button, Icon } from "@real-estate/ui-kit";
+import "@real-estate/ui-kit/index.css"
 
 const App = () => (
   <div className="container">
@@ -9,8 +9,15 @@ const App = () => (
     <div>Framework: react</div>
     <div>Language: TypeScript</div>
     <div>CSS: Empty CSS</div>
+    <div>
+      <Button>Hello</Button>
+    </div>
+    <div>
+      <Icon.CiStar />
+    </div>
   </div>
-)
+);
+
 const rootElement = document.getElementById('app')
 if (!rootElement) throw new Error('Failed to find the root element')
 
